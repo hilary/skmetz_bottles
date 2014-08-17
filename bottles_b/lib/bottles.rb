ec2a141428dea6f1511137b1402c6f43a119ab96
Hilary Holz <hilary@hholz.com>
2014-08-17 16:59:26 -0700

    just case 0 left

--- bottles_b/lib/bottles.rb
+++ bottles_b/lib/bottles.rb
@@ -12,13 +12,23 @@ class Bottles
     case number
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake #{pronoun(number)} down and pass it around, #{name(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
+
+  private
+
+    def container(bottle_number)
+      (bottle_number == 1) ? "bottle" : "bottles"
+    end
+
+    def pronoun(bottle_number)
+      bottle_number == 1 ? "it" : "one"
+    end
+
+    def name(bottle_number)
+      bottle_number == 0 ? "no more" : bottle_number
+    end
 
 end
