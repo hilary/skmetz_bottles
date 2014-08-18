4acaedff656bf5529c1b8c9b721972910547d4f3
Hilary Holz <hilary@hholz.com>
2014-08-17 17:21:37 -0700

    dried out strings

--- bottles_b/lib/bottles.rb
+++ bottles_b/lib/bottles.rb
@@ -9,12 +9,13 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    else
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake #{pronoun(number)} down and pass it around, #{name(number - 1)} #{container(number - 1)} of beer on the wall.\n"
-    end
+    current_bn = number
+    next_bn    = next_bottle_number(number)
+
+    "#{name(current_bn).capitalize} #{container(current_bn)} of beer on the wall, " +
+      "#{name(current_bn)} #{container(current_bn)} of beer.\n" +
+      "#{get_beer(current_bn)}, " + 
+      "#{name(next_bn)} #{container(next_bn)} of beer on the wall.\n"
   end
 
   private
@@ -23,12 +24,24 @@ class Bottles
       (bottle_number == 1) ? "bottle" : "bottles"
     end
 
+    def get_beer(bottle_number)
+      if bottle_number == 0
+        "Go to the store and buy some more"
+      else
+        "Take #{pronoun(bottle_number)} down and pass it around"
+      end
+    end
+
     def pronoun(bottle_number)
       bottle_number == 1 ? "it" : "one"
     end
 
     def name(bottle_number)
-      bottle_number == 0 ? "no more" : bottle_number
+      bottle_number == 0 ? 'no more' : bottle_number.to_s
+    end
+
+    def next_bottle_number(bottle_number)
+      bottle_number == 0 ? 99 : bottle_number - 1
     end
 
 end
